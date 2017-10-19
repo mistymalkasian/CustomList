3d9f19e237beb07adaff12622790f45132075e79
Misty Malkasian <andrewandmisty@gmail.com>
2017-10-19 16:16:15 -0500

    First '.add' unit test written

---
+++ ListUnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace ListUnitTest
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Add_Numbers_AddsNumberToList()
+        {
+            //Arrange
+            MyList<int> newList = new MyList<int>();
+            int result = 5;
+
+
+            //Act
+            newList.Add(result);
+
+
+            //Assert
+            Assert.AreEqual<int>(result, newList[0]);
+
+        }
+    }
+}
