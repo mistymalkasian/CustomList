361a2074e7cddb615212a261c7b5768c6859846c
Misty Malkasian <andrewandmisty@gmail.com>
2017-10-23 09:11:06 -0500

    Added Indexer

--- CustomList/MyList.cs
+++ CustomList/MyList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +7,47 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    public class MyList<T>  
+    public class MyList<T> : IEnumerable<T> 
     {
+        T[] List = { };
         T value;
         int count;
         
-
+        public MyList()
+        {
+           
+        }
+        
         public MyList(T value)
         {
             this.value = value;
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return List[index];
+            }
+
+            set
+            {
+                List[index] = value;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)List).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)List).GetEnumerator();
         }
 
         public void Add(T value)
@@ -26,11 +59,5 @@ namespace CustomList
         {
 
         }
-
-        public void Count(T value)
-        {
-
-        }
-       
     }
 }
